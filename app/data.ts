f181514053096ad6456f7b0ed3ee6c9088b90af0
Khoa Tran <khoatdk007@gmail.com>
2025-05-18 16:03:51 +0700

    edit a little bit of my info

--- app/data.ts
+++ app/data.ts
@@ -27,70 +27,46 @@ type SocialLink = {
   link: string
 }
 
-export const EMAIL = ''
+export const EMAIL = 'ktranowl@gmail.com'
 
-export const WEB_TITLE = 'Name - Personal Website'
+export const WEB_TITLE = 'Tran Dang Khoa - Personal Website'
 export const WEB_DESCRIPTION =
-  'A personal website showcasing my work and projects.'
-export const FOOTER_COPYRIGHT = '© 2025 Nguyen Van B.'
-export const FOOTER_LINK = 'https://github.com/username/'
+  'A personal website showcasing my work and projects (CVE analysis).'
+export const FOOTER_COPYRIGHT = '© 2025 Tran Dang Khoa.'
+export const FOOTER_LINK = 'https://github.com/khoatran107/'
 
-export const NAME = 'Nguyen Van B'
-export const JOB_TITLE = 'NASA Frontend Intern'
+export const NAME = 'Tran Dang Khoa'
+export const JOB_TITLE = 'Viettel Security Research Intern'
 export const DESCRIPTION =
-  'I am a passionate designer and developer with a focus on creating beautiful and functional user interfaces. I have experience in design engineering, front-end development, and product design. I love to explore the intersection of design and technology, and I am always looking for new challenges to tackle.'
+  'I am a passionate security researcher. I want to learn the methods to exploit various targets'
 
 export const PROJECTS: Project[] = [
   {
-    name: 'Motion Primitives Pro',
+    name: 'CVE-2025-21756: n-day analysis',
     description:
-      'Advanced components and templates to craft beautiful websites.',
-    link: 'https://pro.motion-primitives.com/',
+      'From a Use-After-Free in VSOCK to Local Privilege Escalation',
+    link: 'https://hackmd.io/m3q40BtyRA2Err50t9qX2g',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4',
     id: 'project1',
-  },
-  {
-    name: 'Motion Primitives',
-    description: 'UI kit to make beautiful, animated interfaces.',
-    link: 'https://motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4',
-    id: 'project2',
   },
 ]
 
 export const WORK_EXPERIENCE: WorkExperience[] = [
   {
-    company: 'Reglazed Studio',
-    title: 'CEO',
-    start: '2024',
+    company: 'Viettel Cyber Security',
+    title: 'Security Researcher Intern',
+    start: '2025',
     end: 'Present',
-    link: 'https://ibelick.com',
+    link: 'https://viettelcybersecurity.com/',
     id: 'work1',
-  },
-  {
-    company: 'Freelance',
-    title: 'Design Engineer',
-    start: '2022',
-    end: '2024',
-    link: 'https://ibelick.com',
-    id: 'work2',
-  },
-  {
-    company: 'Freelance',
-    title: 'Front-end Developer',
-    start: '2017',
-    end: 'Present',
-    link: 'https://ibelick.com',
-    id: 'work3',
   },
 ]
 
 export const BLOG_POSTS: BlogPost[] = [
   {
-    title: 'Exploring the Intersection of Design, AI, and Design Engineering',
-    description: 'How AI is changing the way we design',
+    title: 'How to use Docker in binary exploit challenge',
+    description: 'Automation of docker build and attach in solve.py script',
     link: '/blog/exploring-the-intersection-of-design-ai-and-design-engineering',
     uid: 'blog-1',
   },
@@ -99,6 +75,6 @@ export const BLOG_POSTS: BlogPost[] = [
 export const SOCIAL_LINKS: SocialLink[] = [
   {
     label: 'GitHub',
-    link: 'https://github.com/github',
+    link: 'https://github.com/khoatran107',
   },
 ]
